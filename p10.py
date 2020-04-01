50e3208d1300505ac6b11fd21240f988d6fdc9c4
Ravin <ravinrakholiya559@gmail.com>
2020-04-01 21:00:39 +0530

    add new p10

---
+++ p10.py
@@ -0,0 +1,38 @@
+# Dictionary comprehension
+
+
+
+# square={1:1,2:4,3:9}
+
+square={num:num**2 for num in range(1,11)}
+print(square)                               # o/p: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100}
+
+
+
+
+square1={f"square of {num} is":num**2 for num in range(1,11)}
+print(square1)                              # o/p: {'square of 1 is': 1, 'square of 2 is': 4, 'square of 3 is': 9, 'square of 4 is': 16, 'square of 5 is': 25, 'square of 6 is': 36, 'square of 7 is': 49, 'square of 8 is': 64, 'square of 9 is': 81, 'square of 10 is': 100}
+
+
+for k,v in square1.items():
+    print(f"{k}:{v}")
+
+# o/p:  square of 1 is:1
+#       square of 2 is:4
+#       square of 3 is:9
+#       square of 4 is:16
+#       square of 5 is:25
+#       square of 6 is:36
+#       square of 7 is:49
+#       square of 8 is:64
+#       square of 9 is:81
+#       square of 10 is:100
+
+
+
+
+
+# word counter
+String1="RavinsRi"
+word_count={i:String1.count(i) for i in String1}
+print(word_count)                              # o/p: {'R': 2, 'a': 1, 'v': 1, 'i': 2, 'n': 1, 's': 1}
