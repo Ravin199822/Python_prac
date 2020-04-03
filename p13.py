67f3bf2bfdcfbb0ff20521bfd7214ac0977c464f
Ravin <ravinrakholiya559@gmail.com>
2020-04-03 20:40:55 +0530

    modified P13

--- p13.py
+++ p13.py
@@ -106,3 +106,156 @@ for i in length:                          #      4
 
 length1=list(map(len,l2))
 print(length1)                            # o/p: [3, 4, 5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Filter() Function
+
+
+def is_even(a):
+    return a%2==0
+
+numbers=[3,2,4,7,9,8,0,6,1]
+
+evens=filter(is_even,numbers)
+print(evens)                # o/p: <filter object at 0x032F92B0>
+
+for i in evens:             # o/p: 2
+    print(i)                #      4
+                            #      8
+                            #      0
+                            #      6
+                
+for i in evens:
+    print(i)            # o/p: no output
+
+# Note: map and filter can be iterated one time only. iterator many times possible in list and tuple but only one time in map and filter.
+
+
+
+
+
+                            
+evens1=tuple(filter(is_even,numbers))
+print(evens1)               # o/p: (2, 4, 8, 0, 6)
+
+evens1=list(filter(is_even,numbers))
+print(evens1)               # o/p: [2, 4, 8, 0, 6]
+
+
+evens3=filter(lambda a:a%2==0,numbers)
+print(evens3)               # o/p: <filter object at 0x035F9298>
+print(tuple(filter(lambda a:a%2==0,numbers)))         # o/p: (2, 4, 8, 0, 6)
+
+
+print([i for i in numbers if i%2==0])                 # o/p: [2, 4, 8, 0, 6]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Iterator vs Iterables 
+ 
+numbers1=[1,2,3,4,5]                 # iterables  (list, tuple, String)
+square2=map(lambda a:a**2,numbers1)  # Iterator
+
+print(square2)                       # o/p: <map object at 0x02B99310>
+
+# for i in square2:                    # o/p: 1
+#     print(i)                         #      4
+#                                      #      9
+#                                      #      16
+#                                      #      25
+
+
+
+
+# for i in numbers1:
+#     print(i)
+
+# How for loop works
+# Steps
+# call iter function
+# iter(numbers)-----> iterator
+# next(iter(numbers))
+
+
+# here, we have to use iter func------> iterables
+number_iter=iter(numbers1)
+# print(number_iter)                         # o/p: <list_iterator object at 0x030D9328>
+# print(next(number_iter))                   # o/p: 1
+# print(next(number_iter))                   # o/p: 2
+# print(next(number_iter))                   # o/p: 3
+# print(next(number_iter))                   # o/p: 4
+# print(next(number_iter))                   # o/p: 5
+# print(next(number_iter))                   # o/p: Error (StopIteration)
+
+
+
+# # Here we don't need to use iter func----->iterator
+# print(next(square2))                         # o/p: 1
+# print(next(square2))                         # o/p: 4
+# print(next(square2))                         # o/p: 9
+# print(next(square2))                         # o/p: 16
+# print(next(square2))                         # o/p: 25
+# print(next(square2))                         # o/p: Error (StopIteration)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Zip() function (we can use this function when we want to combine two different list together)
+user_id=["user_1","User_2","user_3"]
+names3=["Ravin","Rupen","Yash"]
+print(zip(user_id,names3))                  # o/p: <zip object at 0x011C2F48>
+print(list(zip(user_id,names3)))            # o/p: [('user_1', 'Ravin'), ('User_2', 'Rupen'), ('user_3', 'Yash')]
+print(tuple(zip(user_id,names3)))           # o/p: (('user_1', 'Ravin'), ('User_2', 'Rupen'), ('user_3', 'Yash'))
+print(dict(zip(user_id,names3)))            # o/p: {'user_1': 'Ravin', 'User_2': 'Rupen', 'user_3': 'Yash'}
+
+
+user_id1=["user_1","User_2"]
+names4=["Ravin","Rupen","Yash"]
+print(list(zip(user_id1,names4)))          # o/p: [('user_1', 'Ravin'), ('User_2', 'Rupen')]
+print(dict(zip(user_id1,names4)))          # o/p: {'user_1': 'Ravin', 'User_2': 'Rupen'}
+
+
+Example=[('a',1),('b',2)]
+print(dict(Example))                      # o/p: {'a': 1, 'b': 2}
+
+
+
+
+l_name=["Rakholiya","Patel","Gami"]
+print(list(zip(user_id,names3,l_name)))  # o/p: [('user_1', 'Ravin', 'Rakholiya'), ('User_2', 'Rupen', 'Patel'), ('user_3', 'Yash', 'Gami')]
+print(dict(zip(user_id,names3,l_name)))  # o/p: Error
